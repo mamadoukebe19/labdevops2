23c06e4e6e71bd5cc8e4119c65e504f64549fad7
mamadoukebe19 <107811928+mamadoukebe19@users.noreply.github.com>
2024-09-19 13:51:36 +0000

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,5 @@
+  GNU nano 5.8                                                                       data.tf                                                                                 
+data "aws_subnet" "selected" {
+  # Remplacez "subnet-12345678" par l'ID de votre sous-réseau
+  id = "subnet-0bb26e04f3f5fd0d2"
+}
